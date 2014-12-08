6cf487a4c12545491a893728fe06c1983f37b35c
volnoboy <volnoboy@gmail.com>
2014-12-08 20:34:37 +0200

    Spring security add password encoder.

--- src/main/java/com/volnoboy/jb/service/InitDbService.java
+++ src/main/java/com/volnoboy/jb/service/InitDbService.java
@@ -9,6 +9,7 @@ import com.volnoboy.jb.repository.ItemRepository;
 import com.volnoboy.jb.repository.RoleRepository;
 import com.volnoboy.jb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -50,7 +51,8 @@ public class InitDbService {
 
         User userAdmin = new User();
         userAdmin.setName("admin");
-	    userAdmin.setPassword("admin");
+	    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	    userAdmin.setPassword(encoder.encode("admin"));
         List<Role> roles = new ArrayList<Role>();
         roles.add(roleAdmin);
         roles.add(roleUser);
