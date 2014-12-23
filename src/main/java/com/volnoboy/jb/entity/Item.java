45bc51f941fe8981af5ab0991a411182f155e8cf
volnoboy <volnoboy@gmail.com>
2014-12-23 15:25:48 +0200

    Character Large OBject

--- src/main/java/com/volnoboy/jb/entity/Item.java
+++ src/main/java/com/volnoboy/jb/entity/Item.java
@@ -1,5 +1,7 @@
 package com.volnoboy.jb.entity;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -61,8 +63,12 @@ public class Item {
     @GeneratedValue
     private Integer id;
 
+    @Column(length = 1000)
     private String title;
 
+    @Lob
+    @Type(type = "org.hibernate.type.StringClobType")
+    @Column(length = Integer.MAX_VALUE)
     private String description;
 
     @ManyToOne
@@ -72,5 +78,6 @@ public class Item {
     @Column(name="published_date")
     private Date publishedDate;
 
+    @Column(length = 1000)
     private String link;
 }
