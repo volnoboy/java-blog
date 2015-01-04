7e7312d86dc4cc4d4d2a232a03b1a5833b132ddf
volnoboy <volnoboy@gmail.com>
2015-01-04 22:03:29 +0200

    Redirect Attribute

--- src/main/java/com/volnoboy/jb/controller/RegisterController.java
+++ src/main/java/com/volnoboy/jb/controller/RegisterController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -30,12 +31,13 @@ public class RegisterController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String doRegister(@Valid @ModelAttribute("user")User user, BindingResult result) {
+	public String doRegister(@Valid @ModelAttribute("user")User user, BindingResult result, RedirectAttributes redirectAttributes) {
 		if(result.hasErrors()) {
 			return"user-register";
 		}
 		userService.save(user);
-		return "redirect:/register.html?success=true";
+		redirectAttributes.addFlashAttribute("success", true);
+		return "redirect:/register.html";
 	}
 
 	@RequestMapping("/available")
